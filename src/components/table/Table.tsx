1a5cb8966d09a4381b7e72de81670b4707d575c3
a-zelenschi <a.zelenschi@gmail.com>
2022-03-23 19:12:42 +0200

    change bg-color

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Tab = () => {
   return (
     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <thead className="text-xs text-gray-700 uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400">
         <tr>
           <th scope="col" className="px-6 py-3">
             NrCrt
